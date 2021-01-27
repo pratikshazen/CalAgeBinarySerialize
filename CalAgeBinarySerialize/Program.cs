06ea3b55e51382addb3fc7b6e3e9090a3ce9d185
Pratiksha.Sidankar <Pratiksha.Sidankar@zensar.com>
2021-01-27 22:35:40 +0530

    Add project files.

---
+++ CalAgeBinarySerialize/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.IO;
+
+namespace CalAgeBinarySerialize
+{
+        [Serializable]
+        class SerializationClass : IDeserializationCallback
+        {
+            public int birthYear;   
+            [NonSerialized]
+
+            public int age;
+            public SerializationClass(int birthYear)
+            {
+                this.birthYear = birthYear;
+            }
+
+            public void OnDeserialization(object sender)
+            {
+                DateTime dateTime = DateTime.Now;
+                age = DateTime.Now.Year - birthYear;
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Enter the Year of Birth :");
+                int year = int.Parse(Console.ReadLine());
+
+                SerializationClass sc = new SerializationClass(year);
+                FileStream fs = new FileStream(@"AgeCal.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(fs, sc);
+                fs.Seek(0, SeekOrigin.Begin);
+
+                SerializationClass s = (SerializationClass)bf.Deserialize(fs);
+
+                Console.WriteLine("Age is : " + s.age);
+
+            }
+        }
+    }
+ 
